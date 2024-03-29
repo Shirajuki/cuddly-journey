3a859cfbb32acb3e6ebcbd3df4bb330411b8fb8d
Jonny Ngo Luong <shirajuki00@gmail.com>
2024-03-29 22:44:00 +0100

    feat: center footer text

--- web/src/components/layout/Footer.tsx
+++ web/src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="dark bg-background text-foreground h-20 flex items-center border-t border-border/40 max-w-screen-2xl mx-auto p-4">
+    <footer className="dark bg-background text-sm text-foreground h-20 flex justify-center items-center border-t border-border/40 max-w-screen-2xl mx-auto p-4">
       <p>Built by Shirajuki. The source code is available on GitHub.</p>
     </footer>
   );
