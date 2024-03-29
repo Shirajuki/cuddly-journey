7757582abaca4dc26392ad72119db92229fd5b37
Jonny Ngo Luong <shirajuki00@gmail.com>
2024-03-29 22:44:55 +0100

    feat: add github repository link to the footer text

--- web/src/components/layout/Footer.tsx
+++ web/src/components/layout/Footer.tsx
@@ -1,7 +1,13 @@
 export default function Footer() {
   return (
     <footer className="dark bg-background text-sm text-foreground h-20 flex justify-center items-center border-t border-border/40 max-w-screen-2xl mx-auto p-4">
-      <p>Built by Shirajuki. The source code is available on GitHub.</p>
+      <p>
+        Built by Shirajuki. The source code is available on{" "}
+        <a className="underline" href="https://github.com/Shirajuki/cuddly-journey">
+          GitHub
+        </a>
+        .
+      </p>
     </footer>
   );
 }
