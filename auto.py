5c3d1762e2d7222bea543129ec4bb98a6b39c8ed
Jonny Ngo Luong <shirajuki00@gmail.com>
2024-05-25 22:49:31 +0200

    chore: move auto script to root location

---
+++ auto.py
@@ -0,0 +1,13 @@
+import os
+for i in range(1,25):
+    i = str(i).rjust(2, "0")
+    payload = f"python3 ./tts_srt_parsing/process_srt.py ./apoth/Kusuriya\ no\ Hitorigoto\ -\ {i}.srt False False && cp ./scripts/output/subbed.srt ep{i}.srt && cp ./scripts/output/filtered.srt ep{i}_filter.srt"
+    print(payload)
+    os.system(payload)
+    payload = f"python3 ./scripts/tts_srt_parsing/tts-edge.py ep{i}.srt && powershell.exe -c '[console]::beep(500,300)'; python3 ./scripts/process_video/standalone.py && cp ./scripts/output/output.mp3 ep{i}.mp3 && powershell.exe -c '[console]::beep(500,300)'"
+    print(payload)
+    os.system(payload)
+    os.system("rm -rf ./scripts/output/*.mp3")
+    os.system("rm -rf ./scripts/output/*.srt")
+
+
